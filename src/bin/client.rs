7b30fc66522b5c22c003cefffde02f4a38f55538
NuminousBlue <jdiehl2236@gmail.com>
2023-05-13 22:27:33 -0500

    multiplex over oneshot channel with command enum

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,36 +1,83 @@
-use mini_redis::client;
 use bytes::Bytes;
-use tokio::sync::mpsc;
+use mini_redis::client::{self, Client};
+use tokio::sync::{mpsc, oneshot};
 
 #[derive(Debug)]
-
 enum Command {
     Get {
-        key: String
+        key: String,
+        resp: Responder<Option<Bytes>>,
     },
     Set {
         key: String,
-        val: Bytes
-    }
-    
+        val: Bytes,
+        resp: Responder<()>,
+    },
 }
+
+type Responder<T> = oneshot::Sender<mini_redis::Result<T>>;
 
 #[tokio::main]
 async fn main() {
-   
     let (tx, mut rx) = mpsc::channel(32);
 
     let tx2 = tx.clone();
 
-    tokio::spawn(async move {
-        tx.send("Sent from sender1").await;
+
+    let task_1 = tokio::spawn(async move {
+        let (resp_tx, resp_rx) = oneshot::channel();
+
+        let get_cmd = Command::Get {
+            key: "foo".to_string(),
+            resp: resp_tx,
+        };
+
+        tx.send(get_cmd).await.unwrap();
+
+        let res = resp_rx.await;
+        println!("Received response from task_1::: {:#?}", res);
     });
 
-    tokio::spawn(async move {
-        tx2.send("Sent from sender2").await;
+    let task_2 = tokio::spawn(async move {
+        let (resp_tx, resp_rx) = oneshot::channel();
+        let set_cmd = Command::Set {
+            key: "foo".to_string(),
+            val: "bar".into(),
+            resp: resp_tx,
+        };
+
+        tx2.send(set_cmd).await.unwrap();
+
+        let res = resp_rx.await;
+        println!("Received response from task_2 ::: {:#?}", res);
     });
 
     while let Some(msg) = rx.recv().await {
-        println!("Message received ::: {}", msg);
+        println!("Message received ::: {:#?}", msg);
+    }
+
+    let manager = tokio::spawn(async move {
+        let mut client = client::connect("127.0.0.1:6379").await.unwrap();
+
+        while let Some(cmd) = rx.recv().await {
+            by_command(cmd, &mut client).await;
+        }
+    });
+
+    task_1.await.unwrap();
+    task_2.await.unwrap();
+    manager.await.unwrap();
+}
+
+async fn by_command(cmd: Command, client: &mut Client) {
+    match cmd {
+        Command::Get { key, resp } => {
+            let res = client.get(&key).await;
+            let _ = resp.send(res);
+        }
+        Command::Set { key, val, resp } => {
+            let res = client.set(&key, val.clone()).await;
+            let _ = resp.send(res);
+        }
     }
 }
